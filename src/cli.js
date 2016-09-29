5e060475520218a8d39502d896a08907283f2b54
Niklas Närhinen <niklas@narhinen.net>
2016-09-29 22:27:25 +0300

    Working login

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,45 @@
 #!/usr/bin/env node
 
+/* eslint no-console: "" */
+
 import {
   login
 } from './index';
 
 import minimist from 'minimist';
+import prompt from 'cli-prompt';
+import osenv from 'osenv';
+import fs from 'mz/fs';
+import path from 'path';
 
-console.log('here');
+const argv = minimist(process.argv.slice(2));
+
+const confFile = path.join(osenv.home(), '.flowplayerrc');
+
+function loginFlow() {
+  prompt.multi([{
+    key: 'username',
+    label: 'Username / E-mail'
+  }, {
+    key: 'password',
+    label: 'Password'
+  }], ({username, password}) => {
+    login(username, password).then(user => {
+      return fs.writeFile(confFile, JSON.stringify({authcode: user.authcode})).then(() => {
+        console.log(`Logged in as ${user.email}`);
+      }).catch(err => {
+        console.error(`Unable to write to ${confFile}`, err);
+      });
+    }).catch(() => console.log('Login failed'));
+  });
+
+}
+
+(() => {
+  let {
+    _: [command, ...args]
+  } = argv;
+  if (!command) return usageFlow();
+  if (command === 'login') return loginFlow();
+  if (command === 'upload') return uploadFlow(args, argv);
+})();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import fetch from 'node-fetch';
+
 export function login(username, password) {
-  console.log('Would login');
+  return fetch('https://drive.api.dev.flowplayer.org/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username, password})
+  }).then(resp => {
+    if (!resp.ok) throw new Error('Login unsuccessfull');
+    return resp.json().then(json => json.user);
+  });
 }
